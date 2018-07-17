59badfff88b5d58424649c68023a0a674b615b7e
Rodolfo Torres Vega <fito.torres94@gmail.com>
2018-07-16 22:17:56 -0500

    Initial Commit
    Pseudo code with help of
    https://matthewcranford.com/arcade-game-walkthrough-part-2-pseudo-code/

--- js/app.js
+++ js/app.js
@@ -3,6 +3,9 @@ var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
+    // x position
+    // y position
+
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -14,6 +17,12 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
+    // If enemy is not passed boundary
+        // Move forward
+        // Increment x by speed * dt
+    // Else
+        // Reset x and y to starting position
 };
 
 // Draw the enemy on the screen, required method for game
@@ -25,12 +34,32 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+/*Hero
+    Constructor
+        Props
+            x coordinate
+            y coordinate
+        Methods
+            update
+                check for collision with enemy
+                    did hero x any y crash with enemy
+                check if hero won
+                    did hero x any y match final row
+            render
+                draw sprite on x and y hero coordinates
+            handleInput
+                update players x and y according to keyboard input
+            reset hero
+                reset hero x and y to starting x and y
+*/
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-
+// Initialize new hero called player
+// Initialize array called allEnemies
+//     Push each new enemy onto above array
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
